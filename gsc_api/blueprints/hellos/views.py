ebec9a064da401c9583d1191c9a8c2031932323a
Hangry <hangrylydevour@gmail.com>
2022-05-31 23:15:38 +0800

    added inactive hello views

--- gsc_api/blueprints/hellos/views.py
+++ gsc_api/blueprints/hellos/views.py
@@ -211,3 +211,31 @@ def active():
             response.append(data)
 
     return jsonify(response)
+
+@hellos_api_blueprint.route('/inactive', methods=['GET'])
+def inactive():
+    hellos = Hello.select()
+
+    count = 0
+    response = []
+
+    for hello in hellos:
+
+        said_hi = hello.said_hi
+        hi_recipient = hello.hi_recipient
+
+        if hello.contacted == True or hello.removed == True:
+            count += 1
+            data = {
+                "count": count,
+                "hello_id": hello.id,
+                "contacted": hello.contacted,
+                "removed": hello.removed,
+                "said_hi": said_hi.name,
+                "hi_recipient": hi_recipient.name,
+                "hi_recipient_notification_frequency": hi_recipient.notification_frequency
+            }
+            
+            response.append(data)
+
+    return jsonify(response)
