e4001ace20c945ad6221b0a9cf405b4f28fafed9
Hangry <hangrylydevour@gmail.com>
2023-01-23 20:05:17 -0600

    changed parse db url in database.py

--- database.py
+++ database.py
@@ -1,18 +1,24 @@
 import os
 from urllib.parse import urlparse
 
-def parse_db_url(database_url):
-    parsed = urlparse(database_url)
+def parse_db_url():
+    #parsed = urlparse(database_url)
     return {
-        'user': parsed.username,
-        'password': parsed.password,
-        'host': parsed.hostname,
-        'port': parsed.port,
-        'database': parsed.path[1:]
+        #'user': parsed.username,
+        #'password': parsed.password,
+        #'host': parsed.hostname,
+        #'port': parsed.port,
+        #'database': parsed.path[1:]
+        'user': os.environ['USER'],
+        'password': os.environ['PASSWORD'],
+        'host': os.environ['DATABASE_URL'],
+        'port': os.environ['PORT'],
+        'database': os.environ['DBNAME']
     }
 
 def return_db():
-    db_config = parse_db_url(os.environ['DATABASE_URL'])
+    #db_config = parse_db_url(os.environ['DATABASE_URL'])
+    db_config = parse_db_url()
 
     if os.getenv('MIGRATION', '0') == '1':
         from playhouse.postgres_ext import PostgresqlExtDatabase
